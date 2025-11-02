50e90d1fcda4591e34b9003db18df0d4c7069199
Jose Perez <joseperu2503@gmail.com>
2025-11-02 15:27:59 -0500

    back

--- src/auth/dto/check-account-request.dto.ts
+++ src/auth/dto/check-account-request.dto.ts
@@ -1,4 +1,4 @@
-import { OmitType } from '@nestjs/swagger';
+import { OmitType } from '@nestjs/mapped-types';
 import { LoginRequestDto } from './login-request.dto';
 
 export class CheckAccountRequestDto extends OmitType(LoginRequestDto, [
